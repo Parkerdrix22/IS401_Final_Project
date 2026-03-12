2c5c7bdf933ae619a0c593c405c644588e5c0cef
hirohito <hirohito1742@gmail.com>
2026-03-12 15:02:31 -0600

    Revert "add little bit"
    This reverts commit 4652e7c851c8ec76e83b3e09008c5a465da36209.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,6 @@ import app from './app';
 
 const PORT = process.env.PORT ?? 3000;
 
-app.get("/", (req, res) => {
-  res.send("Healthy Habits API is running");
-});
-
-app.get("/api/test", (req, res) => {
-  res.json({ message: "API working" });
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
